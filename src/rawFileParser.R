a96771eedda69aec893add11e576167d38083523
Wenzhao Xu <xuwz.thu@gmail.com>
2017-11-22 02:10:25 -0600

    code to consider MEP and larger than 2mm when parsing LOPC

--- src/rawFileParser.R
+++ src/rawFileParser.R
@@ -58,9 +58,13 @@ distanceSpeedCalculation <- function(data){
 
 LOPCFeature<-function(data){
   # Calculate the LOPC biomass and biodensity data
-    AveFlowCnt <- data$DeltaTime/data$FlowCounts
+    AveFlowCnt <- data$DeltaTime/data$FlowCounts # calculate the average flow time counts
   
-    data$flowSpeed=ifelse(AveFlowCnt<=13,23.10410966*exp(-(-1.481499106*sqrt(sqrt(AveFlowCnt))+1.566460406*sqrt(AveFlowCnt)+0.196311142*sqrt(AveFlowCnt)^2-0.05*sqrt(AveFlowCnt)^3)),0.198996019*exp(-(-2.603059062*sqrt(sqrt(AveFlowCnt))+0.892897609*sqrt(AveFlowCnt)+0.006191239*sqrt(AveFlowCnt)^2-0.0013*sqrt(AveFlowCnt)^3)));
+    data$flowSpeed=ifelse(
+    	AveFlowCnt<=13,
+    	23.10410966*exp(-(-1.481499106*sqrt(sqrt(AveFlowCnt))+1.566460406*sqrt(AveFlowCnt)+0.196311142*sqrt(AveFlowCnt)^2-0.05*sqrt(AveFlowCnt)^3)),
+    	0.198996019*exp(-(-2.603059062*sqrt(sqrt(AveFlowCnt))+0.892897609*sqrt(AveFlowCnt)+0.006191239*sqrt(AveFlowCnt)^2-0.0013*sqrt(AveFlowCnt)^3)));
+    
     numPoints=nrow(data)
 
     if("distance" %in% names(data)){
@@ -71,12 +75,12 @@ LOPCFeature<-function(data){
 
    	LOPCname <- paste("BIN",10:128,sep="")
     
-    dia=seq(150,1920,15);
-    ovolm=pi/6*dia^3/(2.585^2*10^6);
-    numSum=rowSums(data[,LOPCname]);
-    density=numSum/(averageSpeed*1000);
-    biomass=as.matrix(data[,LOPCname])%*%ovolm/(averageSpeed*1000);
-  
+    dia <- seq(150,1920,15);
+    ovolm <- pi/6*dia^3/(2.585^2*10^6);
+    numSum <- rowSums(data[,LOPCname]);
+    density <- numSum/(averageSpeed*1000);
+    biomass <- as.matrix(data[,LOPCname])%*%ovolm/(averageSpeed*1000);
+
     return(list(density=density,biomass=biomass,flowSpeed=data$flowSpeed));
 }
 
@@ -90,60 +94,76 @@ readingRawFile <- function(filename){
 	sampleDate <- lines[[2]][c(4,5,6)]
 	sampleDate <- as.Date(paste(sampleDate,collapse = "_"),format = "%B_%d_%Y")
 	
-	data <- list()
-	geo_Line <- rep(NA,3)
-	Seabird_line <- rep(NA,7) # previous is 10
-	Fluoroprobe_Line <- rep(NA,18)
-	Phytoflash_Line <- rep(NA,7)
-	LOPC_line <- rep(NA,138)
-	i <- 1
+	
 
 	# dfNames <- c("UTC","latitude","longitude","scan.count","pressure","depth","temp","cond","DO.43.mg.L","DO43...sat","DO.optode","optode.T","BAT","fpb_year","fpb_month","fpb_day","fpb_hour","fpb_min","fpb_second","depth.1","temp.1","green","bluegreen","diatom","crypto","unused","unused.1","unused.2","YS","total","transmission","pf_date","pf_time","Fo","Fm","blank","Fv","Yield","BIN1","BIN2","BIN3","BIN4","BIN5","BIN6","BIN7","BIN8","BIN9","BIN10","BIN11","BIN12","BIN13","BIN14","BIN15","BIN16","BIN17","BIN18","BIN19","BIN20","BIN21","BIN22","BIN23","BIN24","BIN25","BIN26","BIN27","BIN28","BIN29","BIN30","BIN31","BIN32","BIN33","BIN34","BIN35","BIN36","BIN37","BIN38","BIN39","BIN40","BIN41","BIN42","BIN43","BIN44","BIN45","BIN46","BIN47","BIN48","BIN49","BIN50","BIN51","BIN52","BIN53","BIN54","BIN55","BIN56","BIN57","BIN58","BIN59","BIN60","BIN61","BIN62","BIN63","BIN64","BIN65","BIN66","BIN67","BIN68","BIN69","BIN70","BIN71","BIN72","BIN73","BIN74","BIN75","BIN76","BIN77","BIN78","BIN79","BIN80","BIN81","BIN82","BIN83","BIN84","BIN85","BIN86","BIN87","BIN88","BIN89","BIN90","BIN91","BIN92","BIN93","BIN94","BIN95","BIN96","BIN97","BIN98","BIN99","BIN100","BIN101","BIN102","BIN103","BIN104","BIN105","BIN106","BIN107","BIN108","BIN109","BIN110","BIN111","BIN112","BIN113","BIN114","BIN115","BIN116","BIN117","BIN118","BIN119","BIN120","BIN121","BIN122","BIN123","BIN124","BIN125","BIN126","BIN127","BIN128","Snapshot","threshold","SampleNumber","FlowCounts","DeltaTime","BufferOverrun","LaserMonitor","ElectronicCounts","CountPeriod","LaserVoltage")
-	dfNames <- c("UTC","latitude","longitude","scan.count","depth","temp","cond","BAT","DO.43.mg.L","DO43...sat","fpb_year","fpb_month","fpb_day","fpb_hour","fpb_min","fpb_second","depth.1","temp.1","green","bluegreen","diatom","crypto","unused","unused.1","unused.2","YS","total","transmission","pf_date","pf_time","Fo","Fm","blank","Fv","Yield","BIN1","BIN2","BIN3","BIN4","BIN5","BIN6","BIN7","BIN8","BIN9","BIN10","BIN11","BIN12","BIN13","BIN14","BIN15","BIN16","BIN17","BIN18","BIN19","BIN20","BIN21","BIN22","BIN23","BIN24","BIN25","BIN26","BIN27","BIN28","BIN29","BIN30","BIN31","BIN32","BIN33","BIN34","BIN35","BIN36","BIN37","BIN38","BIN39","BIN40","BIN41","BIN42","BIN43","BIN44","BIN45","BIN46","BIN47","BIN48","BIN49","BIN50","BIN51","BIN52","BIN53","BIN54","BIN55","BIN56","BIN57","BIN58","BIN59","BIN60","BIN61","BIN62","BIN63","BIN64","BIN65","BIN66","BIN67","BIN68","BIN69","BIN70","BIN71","BIN72","BIN73","BIN74","BIN75","BIN76","BIN77","BIN78","BIN79","BIN80","BIN81","BIN82","BIN83","BIN84","BIN85","BIN86","BIN87","BIN88","BIN89","BIN90","BIN91","BIN92","BIN93","BIN94","BIN95","BIN96","BIN97","BIN98","BIN99","BIN100","BIN101","BIN102","BIN103","BIN104","BIN105","BIN106","BIN107","BIN108","BIN109","BIN110","BIN111","BIN112","BIN113","BIN114","BIN115","BIN116","BIN117","BIN118","BIN119","BIN120","BIN121","BIN122","BIN123","BIN124","BIN125","BIN126","BIN127","BIN128","Snapshot","threshold","SampleNumber","FlowCounts","DeltaTime","BufferOverrun","LaserMonitor","ElectronicCounts","CountPeriod","LaserVoltage")
 	
-		# "BBE_time","Phyto_time","timePassSecond")
+
+	GPGGA_line_names <- c("UTC","latitude","longitude")
+	Seabird_line_names <- c("scan.count","depth","temp","cond","BAT","DO.43.mg.L","DO43...sat")
+	Fluoroprobe_line_names <- c("fpb_year","fpb_month","fpb_day","fpb_hour","fpb_min","fpb_second", "depth.1","temp.1","green","bluegreen","diatom","crypto","unused","unused.1","unused.2","YS","total","transmission")
+	Phytoflash_line_names <- c("pf_date","pf_time","Fo","Fm","blank","Fv","Yield")
+	LOPC_line_names <- c(sapply(1:128, function(x) paste0("BIN",x)),"size_gt_2mm","Snapshot","threshold","SampleNumber","FlowCounts","DeltaTime","BufferOverrun","LaserMonitor","ElectronicCounts","CountPeriod","LaserVoltage")
+	dfNames <- c(GPGGA_line_names, Seabird_line_names, Fluoroprobe_line_names, Phytoflash_line_names, LOPC_line_names)
+	
+	num_GPGGA_values <- length(GPGGA_line_names)
+	num_Seabird_values <- length(Seabird_line_names)
+	num_Fluoroprobe_values <- length(Fluoroprobe_line_names)
+	num_Phytoflash_values <- length(Phytoflash_line_names)
+	num_LOPC_values <- length(LOPC_line_names)
+
+	data <- list()
+	geo_Line <- rep(NA, num_GPGGA_values)
+	Seabird_line <- rep(NA, num_Seabird_values) # previous is 10
+	Fluoroprobe_Line <- rep(NA, num_Fluoroprobe_values)
+	Phytoflash_Line <- rep(NA, num_Phytoflash_values)
+	LOPC_line <- rep(NA, num_LOPC_values)
+	i <- 1
+
 	SeabirdCount = 0
 	LOPCCount = 0
 	PhytoflashCount = 0
 	FluoroprobeCount = 0
 	geoCount = 0
-	newLOPC <- FALSE
+	completeLOPC <- FALSE
+	has_MEP_DS <- FALSE
 
 	for(line in lines){
 		# Seabird Data
 		if("L1" %in% line){
-			if(length(LOPC_line)==138){
+			if(completeLOPC){
+				# if all LOPC data are collected, save the data
 				data[[i]] <- c(geo_Line,Seabird_line,Fluoroprobe_Line,Phytoflash_Line,LOPC_line)
 				i=i+1
 			}
-			LOPC_line <- c(line[-1])
+			completeLOPC <- FALSE
+			LOPC_line <- rep(0, num_LOPC_values)
+			LOPC_line[1:32] <- as.numeric(line[-1])
 		}
 		else if("L2" %in% line){
-			LOPC_line <- c(LOPC_line,line[-1])
+			LOPC_line[33:64] <- as.numeric(line[-1])
 		}
 		else if("L3" %in% line){
-			LOPC_line <- c(LOPC_line,line[-1])
+			LOPC_line[65:96] <- as.numeric(line[-1])
 		}
 		else if("L4" %in% line){
-			LOPC_line <- c(LOPC_line,line[-1])
+			LOPC_line[97:128] <- as.numeric(line[-1])
 		}
 		else if("L5" %in% line){
-			LOPC_line <- c(LOPC_line,line[-1])
+			LOPC_line[130:num_LOPC_values] = as.numeric(line[-1])
 			LOPCCount <- LOPCCount+1
-			newLOPC <- TRUE
+			completeLOPC <- TRUE
 		}
-		else if("S" %in% line && length(line) == 8){  # previous file has 11
-			if(newLOPC){
-				Seabird_line <- line[-1]
-				newLOPC <- FALSE
-				SeabirdCount <- SeabirdCount+1
-			}
+		else if("S" %in% line && length(line) == (num_Seabird_values + 1) ){ 
+			# update Seabird as frequently as possible
+			Seabird_line <- line[-1]
+			SeabirdCount <- SeabirdCount+1
 		}
-		else if("F" %in% line && length(line) == 19){
+		else if("F" %in% line && length(line) == (num_Fluoroprobe_values + 1)){
 			Fluoroprobe_Line <- line[-1]
 			FluoroprobeCount <- FluoroprobeCount+1
 		}
-		else if("C" %in% line && length(line) == 8){
+		else if("C" %in% line && length(line) == (num_Phytoflash_values + 1)){
 			Phytoflash_Line <- line[-1]
 			PhytoflashCount <- PhytoflashCount+1
 		}
@@ -151,15 +171,41 @@ readingRawFile <- function(filename){
 			geo_Line <- line[-1][c(1,2,4)]
 			geoCount <- geoCount+1
 		}
+
+		# adding MEP line parsing, 11/22
+		else if(line[1] == "M" && length(line) == 5){
+			MEP_line = as.numeric(line[-1])
+
+			if(MEP_line[4] > 32768){ # start the MEP calculation
+				if(has_MEP_DS && completeLOPC){
+					MEP_esd <- 0.1806059 + 0.00025459*DS -1.0988e-09 * (DS^2) + 9.54e-15 * DS^3
+					bin_idx <- ceiling(MEP_esd * 1e3/15)
+					if(MEP_esd > 2){
+						LOPC_line[129] = LOPC_line[129] + 1
+					}else if(bin_idx < 129){
+						LOPC_line[bin_idx] = LOPC_line[bin_idx] + 1
+					}
+				}
+				has_MEP_DS <- TRUE
+				DS <- MEP_line[4] - 32768
+			}else{
+				if(has_MEP_DS){
+					DS <- DS + MEP_line[4]
+				}
+			}
+		}
+
 	}
+
 	if(SeabirdCount<1){
 		stop("Wrong Seabird Data")
 	}
+
 	data <- as.data.frame(do.call(rbind,data))
 	data <- data[-1,]
 	# data[,2] <- lonlatTransform(data[,2])
 	# data[,3] <- lonlatTransform(data[,3])
-	data[,-c(1,32,33)] <- apply(data[,-c(1,32,33)],2,as.numeric)
+	data[,-c(1,32,33)] <- apply(data[,-c(1,32,33)],2,as.numeric) # transform to numeric
 	names(data) <- dfNames
 	data$DDLat <- lonlatTransform(data$latitude)
 	data$DDLong <- -1*lonlatTransform(data$longitude)
@@ -200,9 +246,6 @@ readingRawFile <- function(filename){
 		print("no geo data")
 		data$timePassSecond <- seq(0,by=0.5,length.out = nrow(data))
 	}
-	
-	
-	
 	
 
 	# assignTime
